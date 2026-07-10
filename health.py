b557ebe74a1a963ccda2e4bae118dac6ce275550
aybelhariridev2-debug <aybelhariridev2@gmail.com>
2026-07-10 12:47:22 +0100

    Create health.py

---
+++ health.py
@@ -0,0 +1,16 @@
+from http.server import HTTPServer, BaseHTTPRequestHandler
+import os
+
+class Handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header("Content-Type", "application/json")
+        self.end_headers()
+        self.wfile.write(b'{"status":"ok"}')
+
+    def log_message(self, format, *args):
+        pass
+
+port = int(os.environ.get("PORT", 8080))
+
+HTTPServer(("0.0.0.0", port), Handler).serve_forever()
