5db68c0807add0b782265e99718f42c06004a361
aybelhariridev2-debug <aybelhariridev2@gmail.com>
2026-07-17 12:14:14 +0100

    Update start.py

--- start.py
+++ start.py
@@ -1,14 +1,1 @@
-import os
-import subprocess
 
-print("Starting Coturn...")
-
-cmd = [
-    "turnserver",
-    "-c",
-    "/app/turnserver.conf",
-    "--log-file",
-    "stdout",
-]
-
-subprocess.run(cmd)
